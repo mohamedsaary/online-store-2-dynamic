3fd10776194052d65de1bcb076bf08b0a9ddeb6c
mohamedsaary <mohamedsaary04@gmail.com>
2020-08-04 13:16:58 +0300

    created the header.php template (without js functionality)

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    include 'templates/header.php';
+?>
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maho's store</title>
+    <link rel="stylesheet" href="css/resetstyle.css">
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    <header>
+        <div id="btn-toggle-menu"></div>
+        <a href="#" class="logo">MAHO</a>
+        <nav>
+            <ul>
+                <li><a href="#">new arrivals</a></li
+                ><li><a href="#">men</a></li
+                ><li><a href="#">women</a></li
+                ><li><a href="#">boys</a></li
+                ><li><a href="#">girls</a></li
+                ><li><a href="#">baby</a></li>
+            </ul>
+        </nav>
+        <div class="header-actions">
+            <div class="search">
+                <img src="icons/search.png" alt="search-icon">
+            </div>
+            <div class="login">
+                <img src="icons/login.png" alt="login-icon">
+            </div>
+            <div class="basket">
+                <img src="icons/basket.png" alt="basket-icon">
+            </div>
+        </div>
+    </header>
+
